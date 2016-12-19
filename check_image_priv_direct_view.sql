1106bd7d147a2afb94f2072e5ec66c1f52cc8cae
Chenghong Li <lichenghongnk@126.com>
2016-12-19 09:09:40 +0800

    添加查询权限的视图

---
+++ check_image_priv_direct_view.sql
@@ -0,0 +1,19 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `check_image_priv_direct` AS
+    SELECT 
+        `user`.`user_id` AS `allowed_user_id`,
+        `salbum_own_picture`.`user_id` AS `owner_id`,
+        `salbum_own_picture`.`album_name` AS `album_name`,
+        `salbum_own_picture`.`filename` AS `filename`
+    FROM
+        (((`user`
+        JOIN `user_belong_group`)
+        JOIN `group`)
+        JOIN `salbum_own_picture`)
+    WHERE
+        ((`user`.`user_id` = `user_belong_group`.`user_id`)
+            AND (`user_belong_group`.`group_id` = `group`.`group_id`)
+            AND (`group`.`group_id` = `salbum_own_picture`.`group_id`))
